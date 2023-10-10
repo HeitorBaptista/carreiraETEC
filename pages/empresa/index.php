186150b3fcd9aaf2fe57f03454a10592f451aa92
LeandroRodriguesL <leandro.lopes31@etec.sp.gov.br>
2023-10-10 13:25:10 -0300

    aula dia 10

--- pages/empresa/index.php
+++ pages/empresa/index.php
@@ -1,10 +1,9 @@
+
 <nav class="navbar sticky-top bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Lista de Empresas</a>
   </div>
 </nav>
-
-
 
 <div class="container-fluid">
     <table class="table"> 
@@ -16,7 +15,7 @@
           </thead>  
           <tbody>
             <?php
-              foreach($controller->lista as $empresa) {
+              foreach($empresas as $empresa) {
             ?>
              <tr>
                 <td><a href="#"><?=$empresa->getNome()?></a></td>
@@ -27,9 +26,10 @@
               }
             ?>
           </tbody>  
-    </table>  
-    
+    </table>    
+
+
     <div id="div-add">
-        <a href="empresa/novo"><i class="fa fa-plus" aria-hidden="true"></i></a>
-    </div>
+       <a href="empresa/novo"><i class="fa fa-plus" aria-hidden="true"></i></a>
+    </div>  
 </div>    
